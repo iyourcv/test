436c97615c4a6640565c212dc1f44b8116c9b17b
Juan Carlos <Juank199819@gmail.com>
2020-07-16 16:12:34 +0200

    cambio script

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 alert("Juan Carlos anormal");  
+
+let d = new Date();
+        document.body.innerHTML = "<h1>Mira lo que sé hacer acho esto es la hora:  " + d.getHours(2) + ":" + d.getMinutes(2) + ":" + d.getSeconds(2)
+        "</h1>"
